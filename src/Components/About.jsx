b8144d662ec5f87ea71efb0c36756d63beb3205a
sakshikharche4 <sakshikharche4@gmail.com>
2026-01-06 13:16:58 +0530

    Update in Projects

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -64,7 +64,7 @@ const About = () => {
               </p>
 
               {/* Cards */}
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-1  gap-6">
                 {aboutInfo.map((data, index) => (
                   <div
                     key={index}
